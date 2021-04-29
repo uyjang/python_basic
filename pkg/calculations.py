9bfc86b8d180ae1b7bdafe3462c5c29d68149bc1
Uiyoung Jang <jsoul52@naver.com>
2021-04-29 21:34:24 +0900

    first added

---
+++ pkg/calculations.py
@@ -0,0 +1,6 @@
+def add(l,r):
+    return l+r
+def mul(l,r):
+    return l*r
+def div(l,r):
+    return l/r
---
+++ pkg/prints.py
@@ -0,0 +1,10 @@
+def prt1():
+    print("I'm Niceboy!")
+def prt2():
+    print("I'm Goodboy!")
+
+# python 2.x 버전의 경우 아래의 코드로 테스트를 해보고 모듈로 사용한다.
+if __name__ == "__main__":
+    prt1()
+    prt2()
+ 
